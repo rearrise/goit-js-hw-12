8157fa0cb41b4b70548a635ff5d93c060e1dd7fb
Danylo <tlink.hitech@gmail.com>
2026-06-21 21:37:05 +0700

    Fix main.js

--- src/main.js
+++ src/main.js
@@ -38,7 +38,7 @@ form.addEventListener('submit', async event => {
     }
 
     createGallery(data.hits);
-    const loadItem = page * 15;
+    const loadedItems = document.querySelectorAll('.gallery-item').length;
     if (loadItem >= data.totalHits) {
       hideLoadMoreButton();
       iziToast.info({
@@ -70,7 +70,7 @@ loadMoreButton.addEventListener('click', async () => {
       return;
     }
     createGallery(data.hits);
-    const loadItem = page * 15;
+    const loadedItems = document.querySelectorAll('.gallery-item').length;
     const image = document.querySelector('.gallery-item');
     const imageHeight = image.getBoundingClientRect().height;
 
